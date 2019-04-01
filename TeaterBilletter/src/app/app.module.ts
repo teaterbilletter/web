0d69a670dc0ee69c82cbc947f931f7cf23e9dc78
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-04-01 15:58:04 +0200

    add navigationbar and footer

--- TeaterBilletter/src/app/app.module.ts
+++ TeaterBilletter/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationbarComponent } from './static_content/navigationbar/navigationbar.component';
+import { FooterComponent } from './static_content/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
