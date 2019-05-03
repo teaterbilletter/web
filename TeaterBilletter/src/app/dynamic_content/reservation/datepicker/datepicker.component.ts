bc19c5604bdb31a6f31b69a82710cad014fb9cf2
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-05-03 16:23:11 +0200

    Get date into ts file

--- TeaterBilletter/src/app/dynamic_content/reservation/datepicker/datepicker.component.ts
+++ TeaterBilletter/src/app/dynamic_content/reservation/datepicker/datepicker.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatepickerComponent implements OnInit {
 
+  public showSeats: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public ShowSeats(date) {
+    this.showSeats = true;
+
+    console.log(date.value);
+
+    return this.showSeats
   }
 
 }
