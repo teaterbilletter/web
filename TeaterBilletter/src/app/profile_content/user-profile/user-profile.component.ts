65f3626044891ac1e67a952d1ef3b17e95fe5aa0
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-05-04 11:31:22 +0200

    Get data from one person - this is hardcoded right now

--- TeaterBilletter/src/app/profile_content/user-profile/user-profile.component.ts
+++ TeaterBilletter/src/app/profile_content/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Customer} from '../../../model/customer';
 
 @Component({
   selector: 'app-user-profile',
@@ -8,12 +10,17 @@ import { Component, OnInit } from '@angular/core';
 export class UserProfileComponent implements OnInit {
 
   public shows : number[];
+  public customer: Customer;
 
-  constructor() {
+  private baseUrl = "https://ticket.northeurope.cloudapp.azure.com/Customer/7";
+
+  constructor(private client: HttpClient) {
     this.shows = [1,2,3,4];
   }
 
   ngOnInit() {
+    this.client.get<Customer>(this.baseUrl).subscribe(customer => {
+      this.customer = customer;
+    });
   }
-
 }
