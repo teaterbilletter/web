dea9661ae9132e8e9d704a8ee4e2d03f37c3dd51
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-05-04 10:05:57 +0200

    Add models from swagger

---
+++ TeaterBilletter/src/model/models.ts
@@ -0,0 +1,7 @@
+export * from './booking';
+export * from './customer';
+export * from './hall';
+export * from './login';
+export * from './seat';
+export * from './show';
+export * from './theater';
