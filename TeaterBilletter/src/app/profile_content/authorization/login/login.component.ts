61b952ea0f10f42a3358142986d8f5b617befc08
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-05-06 21:20:58 +0200

    route to profile if logged in

--- TeaterBilletter/src/app/profile_content/authorization/login/login.component.ts
+++ TeaterBilletter/src/app/profile_content/authorization/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../../auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +9,16 @@ import {AuthService} from '../../../auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
-
-  ngOnInit() {
+  constructor(private authService: AuthService, private router: Router) {
+    if (!this.authService.isTokenExpired(this.authService.getToken())) {
+      this.router.navigate(['/profile']);
+    }
   }
 
+  ngOnInit() { }
+
   public OnLoginPressed(name: string, password: string) {
-    this.authService.login(name, password)
+    this.authService.login(name, password);
+    this.router.navigate(['/profile']);
   }
 }
