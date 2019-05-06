0d205a175c4b66ee3126300964e9839887071b75
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-05-06 21:21:29 +0200

    databind navbar item

--- TeaterBilletter/src/app/static_content/navigationbar/navigationbar.component.ts
+++ TeaterBilletter/src/app/static_content/navigationbar/navigationbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../auth.service';
 
 @Component({
   selector: 'app-navigationbar',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationbarComponent implements OnInit {
 
-  constructor() { }
+  private isLoggedIn;
+
+  public itemText: string;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
-  }
+    this.isLoggedIn = this.authService.isTokenExpired(this.authService.getToken());
 
+    this.itemText = this.isLoggedIn ? 'LOG IN' : 'PROFILE';
+  }
 }
