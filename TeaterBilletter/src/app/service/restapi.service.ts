42be200a370241b585287a13725b2b170322ca8c
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-05-07 11:10:49 +0200

    Update restapi.service.ts

--- TeaterBilletter/src/app/service/restapi.service.ts
+++ TeaterBilletter/src/app/service/restapi.service.ts
@@ -3,7 +3,7 @@ export class RestapiService {
   public baseUrl: string = "https://ticket.northeurope.cloudapp.azure.com:5443/";
 
   public authUrl(): string {
-    return this.baseUrl.concat('Login');
+    return this.baseUrl.concat('UserLogin');
   }
 
   public showUrl(): string {
