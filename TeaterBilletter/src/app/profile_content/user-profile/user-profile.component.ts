24ae863e175701e86613a7ddec7c21af768a63c4
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-05-07 12:51:05 +0200

    Get customer

--- TeaterBilletter/src/app/profile_content/user-profile/user-profile.component.ts
+++ TeaterBilletter/src/app/profile_content/user-profile/user-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Customer} from '../../../model/customer';
+import {RestapiService} from '../../service/restapi.service';
+import {AuthService} from '../../auth.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -12,14 +14,12 @@ export class UserProfileComponent implements OnInit {
   public shows : number[];
   public customer: Customer;
 
-  private baseUrl = "https://ticket.northeurope.cloudapp.azure.com:5443/Customer/4";
-
-  constructor(private client: HttpClient) {
+  constructor(private client: HttpClient, private restapi: RestapiService, private authService: AuthService) {
     this.shows = [1,2,3,4];
   }
 
   ngOnInit() {
-    this.client.get<Customer>(this.baseUrl).subscribe(customer => {
+    this.client.get<Customer>(this.restapi.customerUrl().concat(this.authService.getUserId())).subscribe(customer => {
       this.customer = customer;
     });
   }
