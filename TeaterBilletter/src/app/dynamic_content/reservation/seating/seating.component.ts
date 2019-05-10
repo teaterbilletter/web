141a6ae557d970a048dd06eeb45f16ded380474a
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-05-10 11:32:43 +0200

    logic for seating

--- TeaterBilletter/src/app/dynamic_content/reservation/seating/seating.component.ts
+++ TeaterBilletter/src/app/dynamic_content/reservation/seating/seating.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {SeatingService} from '../../../service/seating.service';
 import {Seat} from '../../../../model/seat';
 import {ShowService} from '../../../service/show.service';
+import {BookingService} from '../../../service/booking.service';
 
 declare var $: any;
 
@@ -17,7 +18,7 @@ export class SeatingComponent implements OnInit {
   public chosenSeats: Map<string, boolean> = new Map<string, boolean>();
   public seatClicked;
 
-  constructor(private seatingService: SeatingService, private showService: ShowService) {
+  constructor(private seatingService: SeatingService, private showService: ShowService, private bookingService: BookingService) {
 
    }
 
@@ -28,14 +29,27 @@ export class SeatingComponent implements OnInit {
 
       this.chosenSeats = new Map();
 
+      let arrayOfAllSeats = new Array();
+
       for (let i = 0; i < this.rows.length; i++) {
         for (let j = 0; j < this.seats.length; j++) {
           var row = i.toString();
           var seat = j.toString();
           var id = row.concat(seat);
           this.chosenSeats.set(id, false);
+
+          let seatForMap: Seat = new class implements Seat {
+            occupied: boolean = false;
+            row_number: number = i;
+            seat_number: number = j;
+          }
+
+          arrayOfAllSeats.push(seatForMap);
         }
       }
+
+      this.seatingService.setOccupiedSeats(arrayOfAllSeats);
+
     }, 500);
   }
 
@@ -68,15 +82,21 @@ export class SeatingComponent implements OnInit {
     var color;
 
 
-    if (this.seatingService.getOccupiedSeatMap().get(seatId) == null) {
+    if (this.seatingService.getOccupiedSeatMap().get(seatId) == null || !this.seatingService.getOccupiedSeatMap().get(seatId).occupied) {
       if (this.chosenSeats.get(seatId)) {
         this.chosenSeats.set(seatId, false);
         color = "linear-gradient(to top, #2E7D32, #388E3C, #43A047, #4CAF50, #66BB6A, #81C784,  #A5D6A7)";
         this.showService.numberOfTickets--;
+
+        // Remove element from Arrat
+        let seatToRemove = this.seatingService.getOccupiedSeatMap().get(seatId);
+        this.bookingService.seats = this.bookingService.seats.filter(obj => obj !== seatToRemove);
+
       } else {
         this.chosenSeats.set(seatId, true);
         color = "linear-gradient(to top, #761818, #761818, #761818, #761818, #761818, #B54041,  #F3686A)";
         this.showService.numberOfTickets++;
+        this.bookingService.seats.push(this.seatingService.getOccupiedSeatMap().get(seatId));
       }
 
       $(jqueryElementId).css("background", color);
--- TeaterBilletter/src/app/service/booking.service.ts
+++ TeaterBilletter/src/app/service/booking.service.ts
@@ -1,6 +1,8 @@
 import {Show} from '../../model/show';
+import {Seat} from '../../model/seat';
 
 export class BookingService {
   public date: string;
   public show: Show;
+  public seats: Array<Seat> = new Array<Seat>();
 }
