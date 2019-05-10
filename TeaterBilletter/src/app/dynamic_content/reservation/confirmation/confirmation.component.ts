4b2ae3d06cee49202493ddd53cae3d68244606d4
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-05-10 10:14:26 +0200

    Update confirmation.component.ts

--- TeaterBilletter/src/app/dynamic_content/reservation/confirmation/confirmation.component.ts
+++ TeaterBilletter/src/app/dynamic_content/reservation/confirmation/confirmation.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {ShowService} from '../../../service/show.service';
+import {AuthService} from '../../../auth.service';
+import {Booking} from '../../../../model/booking';
+import {Seat} from '../../../../model/seat';
+import {Show} from '../../../../model/show';
+import {BookingService} from '../../../service/booking.service';
 
 @Component({
   selector: 'app-confirmation',
@@ -8,9 +13,28 @@ import {ShowService} from '../../../service/show.service';
 })
 export class ConfirmationComponent implements OnInit {
 
-  constructor(private showService: ShowService) { }
+  constructor(private showService: ShowService, private authService: AuthService, private bookingService: BookingService) { }
 
   ngOnInit() {
   }
 
+  onPaymentButtonClicked() {
+    let booking: Booking = new class implements Booking {
+      bookingID: number;
+      customerID: string;
+      date: Date;
+      seats: Array<Seat>;
+      show: Show;
+      totalPrice: number;
+    }
+
+    booking.bookingID = 123;
+    booking.customerID = "s155576";
+    booking.date = new Date(Date.parse(this.bookingService.date));
+    booking.show = this.bookingService.show;
+    booking.totalPrice = 500;
+
+    let json = JSON.stringify(booking);
+    console.log("Json is: " + json);
+  }
 }
