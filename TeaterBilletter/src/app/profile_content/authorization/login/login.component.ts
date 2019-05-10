e456fd93af8ba4f7526d90ae67abc6a566a45fb4
Jaafar Mahdi <jafa@bang-olufsen.dk>
2019-05-10 09:25:04 +0200

    Make login work in pop up too

--- TeaterBilletter/src/app/profile_content/authorization/login/login.component.ts
+++ TeaterBilletter/src/app/profile_content/authorization/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../../../auth.service';
 import {Router} from '@angular/router';
+import {VisualComponentService} from '../../../visualComponent.service';
 
 @Component({
   selector: 'app-login',
@@ -9,15 +10,15 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) {
-    if (!this.authService.isTokenExpired(this.authService.getToken())) {
+  constructor(private authService: AuthService, private router: Router, private visualService: VisualComponentService) {
+    if (!this.authService.isTokenExpired(this.authService.getToken()) && this.visualService.getConfirm()) {
       this.router.navigate(['/profile']);
     }
   }
 
   ngOnInit() { }
 
-  public OnLoginPressed(name: string, password: string) {
-    this.authService.login(name, password);
+  public OnLoginPressed(name: string, password: string, shouldRoute: boolean = true) {
+    this.authService.login(name, password, shouldRoute);
   }
 }
